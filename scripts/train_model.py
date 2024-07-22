cc78e182762822b6e89c943ba06134af0e93b3cc
lauren-yang <yang45248@sas.edu.sg>
2024-07-22 22:27:25 +0800

    Update train_model.py

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -56,4 +56,4 @@ def train_and_save_model(data_path, model_path, imputer_path, scaler_path):
     joblib.dump(scaler, scaler_path)
 
 # Call the function to train and save the model
-train_and_save_model('data/activities.csv', 'models/trained_model.pkl', 'models/imputer.pkl', 'models/scaler.pkl')
+train_and_save_model('../data/activities.csv', 'models/trained_model.pkl', 'models/imputer.pkl', 'models/scaler.pkl')
